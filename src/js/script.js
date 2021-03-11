9543927d4d806a6089cd1b7e6fbfc4563eaab3b0
Marina <badbilliejoe@gmail.com>
2021-03-11 21:55:27 +0300

    second commit

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+      speed: 1200,
+      // adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.png"></button>',
+      responsive: [
+        {
+          breakpoint: 992,
+         settings: {
+            dots: true,
+            arrows: false
+          }
+        }
+      ]
+    });
+  });
